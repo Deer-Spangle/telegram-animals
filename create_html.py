328595d8039daf63bc25209a38683b191fa8ffa9
Deer-Spangle <deer@spangle.org.uk>
2021-02-10 21:48:41 +0000

    Splitting bot table and channel table generation, so that channel table can use cached counts

--- create_html.py
+++ create_html.py
@@ -3,10 +3,46 @@ import os
 
 from yattag import Doc, indent
 
-from data import Channel, load_channels_and_bots
+from data import Channel, load_channels_and_bots, load_channel_cache
 
 
-def build_table(entities: List[Channel], doc):
+def build_channel_table(entities: List[Channel], doc):
+    channel_cache = load_channel_cache()
+    doc, tag, text, line = doc.ttl()
+
+    with tag("table"):
+        with tag("thead"):
+            with tag("tr"):
+                line("th", "Link")
+                line("th", "Animal")
+                line("th", "Owner")
+                line("th", "# pics")
+                line("th", "# gifs")
+                line("th", "# vids")
+                line("th", "Latest post")
+                line("th", "Notes")
+        with tag("tbody"):
+            for entity in entities:
+                with tag("tr"):
+                    with tag("td"):
+                        with tag("a", href=f"https://t.me/{entity.handle}"):
+                            text(f"@{entity.handle}")
+                    line("td", entity.animal)
+                    line("td", entity.owner)
+                    cache = channel_cache.get(entity.handle.casefold())
+                    if cache:
+                        line("td", cache.pic_count)
+                        line("td", cache.gif_count)
+                        line("td", cache.video_count)
+                        latest_str = cache.latest_post.strftime("%Y-%m-%d") if cache.latest_post else "-"
+                        line("td", latest_str)
+                    else:
+                        for _ in range(4):
+                            line("td", "?")
+                    line("td", entity.notes)
+
+
+def build_bot_table(entities: List[Channel], doc):
     doc, tag, text, line = doc.ttl()
 
     with tag("table"):
@@ -39,10 +75,10 @@ def create_doc(channels: List[Channel], bots: List[Channel]):
                 text("Telegram animal channels")
             with tag("p"):
                 text("This site is just a list of telegram channels for pictures, gifs, and videos of animals")
-            build_table(channels, doc)
+            build_channel_table(channels, doc)
             with tag("p"):
                 doc.text("And the table of bots is below")
-            build_table(bots, doc)
+            build_bot_table(bots, doc)
             doc.text("If you would like to request any changes, please file an issue on the ")
             with doc.tag("a", href="https://github.com/Deer-Spangle/telegram-animals/"):
                 text("github repository")
