10aa388cc2f60a779392f3a952e235c9986ed029
Deer-Spangle <deer@spangle.org.uk>
2021-02-10 16:07:15 +0000

    Splitting bots onto their own table

--- create_html.py
+++ create_html.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Callable, Any
+from typing import List, Callable, Any, Tuple
 import json
 import os
 
@@ -24,17 +24,27 @@ class Channel:
             data["handle_pattern"]
         )
 
+    @property
+    def is_bot(self) -> bool:
+        return self.handle.lower().endswith("bot")
 
-def load_channels() -> List[Channel]:
+
+def load_entities() -> Tuple[List[Channel], List[Channel]]:
     with open("channels.json", "r") as f:
-        channel_data_store = json.load(f)
-    return sorted([
+        data_store = json.load(f)
+    sorted_entites = sorted([
         Channel.from_json(channel_data)
-        for channel_data in channel_data_store["channels"]
+        for channel_data in data_store["channels"]
     ], key=lambda c: (c.animal, c.handle))
+    return (
+        [channel for channel in sorted_entites if not channel.is_bot],
+        [bot for bot in sorted_entites if bot.is_bot]
+    )
 
 
-def build_table(channels: List[Channel], tag, text, line):
+def build_table(entities: List[Channel], doc):
+    doc, tag, text, line = doc.ttl()
+
     with tag("table"):
         with tag("thead"):
             with tag("tr"):
@@ -43,17 +53,17 @@ def build_table(channels: List[Channel], tag, text, line):
                 line("th", "Owner")
                 line("th", "Notes")
         with tag("tbody"):
-            for channel in channels:
+            for entity in entities:
                 with tag("tr"):
                     with tag("td"):
-                        with tag("a", href=f"https://t.me/{channel.handle}"):
-                            text(f"@{channel.handle}")
-                    line("td", channel.animal)
-                    line("td", channel.owner)
-                    line("td", channel.notes)
+                        with tag("a", href=f"https://t.me/{entity.handle}"):
+                            text(f"@{entity.handle}")
+                    line("td", entity.animal)
+                    line("td", entity.owner)
+                    line("td", entity.notes)
 
 
-def create_doc(table_builder: Callable[[Any, Any, Any], None]):
+def create_doc(channels: List[Channel], bots: List[Channel]):
     doc, tag, text, line = Doc().ttl()
 
     with tag("html"):
@@ -65,14 +75,16 @@ def create_doc(table_builder: Callable[[Any, Any, Any], None]):
                 text("Telegram animal channels")
             with tag("p"):
                 text("This site is just a list of telegram channels for pictures, gifs, and videos of animals")
-            table_builder(tag, text, line)
+            build_table(channels, doc)
+            with tag("p"):
+                doc.text("And the table of bots is below")
+            build_table(bots, doc)
     return indent(doc.getvalue())
 
 
 if __name__ == "__main__":
-    channel_list = load_channels()
-    table = lambda x, y, z: build_table(channel_list, x, y, z)
-    html = create_doc(table)
+    list_channels, list_bots = load_entities()
+    html = create_doc(list_channels, list_bots)
     os.makedirs("public", exist_ok=True)
     with open("public/index.html", "w") as w:
         w.write(html)
