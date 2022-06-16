b6420277bebb56a1f7bfbab13b6856356c8db0e2
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2022-06-16 02:13:31 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@fb2476956ce6b562c66b2e59814d78b7014238a6 🚀

--- errors.js
+++ errors.js
@@ -3,28 +3,34 @@ const telegramChannels = {
     {
       "handle": "SlothLovers",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-15T02:09:10.266320"
+      "timestamp": "2022-06-16T02:02:44.965692"
     },
     {
       "handle": "bagofcheetos",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-15T02:09:30.448712"
+      "timestamp": "2022-06-16T02:03:08.365541"
     },
     {
       "handle": "thedailycat",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-15T02:11:29.784297"
+      "timestamp": "2022-06-16T02:05:23.349820"
     },
     {
       "handle": "dog_gifs",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-15T02:11:35.882859"
+      "timestamp": "2022-06-16T02:05:26.787050"
     },
     {
       "handle": "dogspic",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-15T02:11:42.926166"
+      "timestamp": "2022-06-16T02:05:34.765737"
     }
   ],
-  "twitter_cache_errors": []
+  "twitter_cache_errors": [
+    {
+      "handle": "HourlyYote",
+      "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
+      "timestamp": "2022-06-16T02:09:06.119080"
+    }
+  ]
 }
