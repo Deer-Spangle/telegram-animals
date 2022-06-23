f284ac0733c7d22b49a9a36a4c5c75ff9f001282
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2022-06-23 02:17:13 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@80fc8affca390d8a7255d3691de9ba5e1b57e72e 🚀

--- errors.js
+++ errors.js
@@ -3,27 +3,27 @@ const telegramChannels = {
     {
       "handle": "SlothLovers",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-22T02:17:45.189723"
+      "timestamp": "2022-06-23T02:06:32.306596"
     },
     {
       "handle": "bagofcheetos",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-22T02:18:05.648525"
+      "timestamp": "2022-06-23T02:06:52.927000"
     },
     {
       "handle": "thedailycat",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-22T02:20:12.983747"
+      "timestamp": "2022-06-23T02:08:55.731015"
     },
     {
       "handle": "dog_gifs",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-22T02:20:15.997654"
+      "timestamp": "2022-06-23T02:09:01.833175"
     },
     {
       "handle": "dogspic",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-06-22T02:20:22.911234"
+      "timestamp": "2022-06-23T02:09:08.839178"
     }
   ],
   "twitter_cache_errors": []
