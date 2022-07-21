7a722314d30acd2fe4adc51c4eafe251ac722517
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2022-07-21 02:27:28 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@de64636921e93e71c28288bfc0689142b86ab6fc 🚀

--- errors.js
+++ errors.js
@@ -3,14 +3,14 @@ const telegramChannels = {
     {
       "handle": "bagofcheetos",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-07-20T02:15:50.502923"
+      "timestamp": "2022-07-21T02:15:50.941336"
     }
   ],
   "twitter_cache_errors": [
     {
       "handle": "batsdotexe",
       "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-07-20T02:22:36.964130"
+      "timestamp": "2022-07-21T02:22:30.442089"
     }
   ]
 }
