9385aad2b39b776f10efbdc1303999e7d93bb635
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2022-08-13 02:12:32 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@dcaae33708965a90690375229a242b6af77f57a4 🚀

--- errors.js
+++ errors.js
@@ -3,24 +3,24 @@ const telegramChannels = {
     {
       "handle": "bagofcheetos",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-08-12T02:08:48.677525"
+      "timestamp": "2022-08-13T02:01:09.284823"
     },
     {
       "handle": "frogs_channel",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-08-12T02:10:40.463740"
+      "timestamp": "2022-08-13T02:03:01.318570"
     }
   ],
   "twitter_cache_errors": [
     {
       "handle": "batsdotexe",
       "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-08-12T02:15:16.225347"
+      "timestamp": "2022-08-13T02:07:26.497970"
     },
     {
       "handle": "PuppiesIover",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2022-08-12T02:19:36.651788"
+      "timestamp": "2022-08-13T02:11:42.541026"
     }
   ]
 }
