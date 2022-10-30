fcf8fe1889e7e674270d8f60e3eee4ade7ed1fec
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2022-10-30 02:30:42 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@7b3a88a9aba1ddd845ef630d9f023f55d09a6fea 🚀

--- errors.js
+++ errors.js
@@ -1,41 +1,31 @@
 const telegramChannels = {
   "telegram_cache_errors": [
     {
-      "handle": "otterdaily",
-      "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-10-29T22:29:46.331458"
-    },
-    {
-      "handle": "dailyrabbit",
-      "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-10-29T22:29:50.880662"
-    },
-    {
       "handle": "bagofcheetos",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-10-29T22:31:16.298715"
+      "timestamp": "2022-10-30T02:30:29.326418"
     }
   ],
   "twitter_cache_errors": [
     {
       "handle": "batsdotexe",
       "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-10-29T22:36:30.363291"
+      "timestamp": "2022-10-30T02:30:32.440753"
     },
     {
       "handle": "standardpuppies",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2022-10-29T22:40:26.148651"
+      "timestamp": "2022-10-30T02:30:34.928925"
     },
     {
       "handle": "PuppiesIover",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2022-10-29T22:40:28.192961"
+      "timestamp": "2022-10-30T02:30:36.968280"
     },
     {
       "handle": "DailyManedWolf",
       "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-10-29T22:40:52.558600"
+      "timestamp": "2022-10-30T02:30:39.047292"
     }
   ]
 }
