0dac04bcff6ab6e771e62265b0d4388769d67f92
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2022-10-31 20:15:11 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@db6bf7e2e555832d4329c639471d83649af0bda7 🚀

--- errors.js
+++ errors.js
@@ -1,31 +1,126 @@
 const telegramChannels = {
   "telegram_cache_errors": [
     {
+      "handle": "tigerpictures",
+      "error": "Cache update failed: Channel tigerpictures no longer has a username",
+      "timestamp": "2022-10-31T20:01:59.626563"
+    },
+    {
+      "handle": "bigcatgifs",
+      "error": "Cache update failed: Channel bigcatgifs no longer has a username",
+      "timestamp": "2022-10-31T20:02:03.209956"
+    },
+    {
+      "handle": "CheetahGifs",
+      "error": "Cache update failed: Channel CheetahGifs no longer has a username",
+      "timestamp": "2022-10-31T20:02:06.882124"
+    },
+    {
+      "handle": "lynx_gifs",
+      "error": "Cache update failed: Channel lynx_gifs no longer has a username",
+      "timestamp": "2022-10-31T20:02:10.544331"
+    },
+    {
+      "handle": "snepgifs",
+      "error": "Cache update failed: Channel snepgifs no longer has a username",
+      "timestamp": "2022-10-31T20:02:14.972153"
+    },
+    {
+      "handle": "BatGifs",
+      "error": "Cache update failed: Channel handle should be BatGifs but it is BatGifArchive",
+      "timestamp": "2022-10-31T20:02:19.507822"
+    },
+    {
+      "handle": "birdpics",
+      "error": "Cache update failed: Channel birdpics no longer has a username",
+      "timestamp": "2022-10-31T20:02:25.132274"
+    },
+    {
+      "handle": "TheFoxNews",
+      "error": "Cache update failed: Channel handle should be TheFoxNews but it is TrueFoxNews",
+      "timestamp": "2022-10-31T20:03:59.673295"
+    },
+    {
+      "handle": "ratsssz",
+      "error": "Cache update failed: Channel handle should be ratsssz but it is ratscapies",
+      "timestamp": "2022-10-31T20:04:38.354935"
+    },
+    {
+      "handle": "StuffAboutCrows",
+      "error": "Cache update failed: Channel handle should be StuffAboutCrows but it is Crowposting",
+      "timestamp": "2022-10-31T20:04:45.316802"
+    },
+    {
       "handle": "bagofcheetos",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by SearchRequest)",
-      "timestamp": "2022-10-31T02:31:16.963547"
+      "timestamp": "2022-10-31T20:04:51.196101"
+    },
+    {
+      "handle": "guineapig_channel",
+      "error": "Cache update failed: Channel handle should be guineapig_channel but it is ann_livee",
+      "timestamp": "2022-10-31T20:05:37.331734"
+    },
+    {
+      "handle": "possumgifs",
+      "error": "Cache update failed: Channel handle should be possumgifs but it is animalicarinii",
+      "timestamp": "2022-10-31T20:05:50.797382"
+    },
+    {
+      "handle": "shibainugif",
+      "error": "Cache update failed: Channel handle should be shibainugif but it is shibaeffepo",
+      "timestamp": "2022-10-31T20:06:00.973226"
+    },
+    {
+      "handle": "pigeonspic",
+      "error": "Cache update failed: Channel handle should be pigeonspic but it is freefixedtip365",
+      "timestamp": "2022-10-31T20:06:19.837711"
+    },
+    {
+      "handle": "pandagif",
+      "error": "Cache update failed: Channel pandagif no longer has a username",
+      "timestamp": "2022-10-31T20:06:24.099267"
+    },
+    {
+      "handle": "catspics",
+      "error": "Cache update failed: Channel handle should be catspics but it is maomaomeow",
+      "timestamp": "2022-10-31T20:06:44.438284"
+    },
+    {
+      "handle": "shiba_gif",
+      "error": "Cache update failed: Channel handle should be shiba_gif but it is PersianAnaliz",
+      "timestamp": "2022-10-31T20:06:58.058040"
+    },
+    {
+      "handle": "foxesposting",
+      "error": "Cache update failed: Channel handle should be foxesposting but it is foxespost",
+      "timestamp": "2022-10-31T20:07:24.846989"
     }
   ],
   "twitter_cache_errors": [
     {
       "handle": "batsdotexe",
       "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-10-31T02:36:54.649562"
+      "timestamp": "2022-10-31T20:10:28.778507"
+    },
+    {
+      "handle": "CuteLilRedBois",
+      "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
+      "timestamp": "2022-10-31T20:14:06.527859"
     },
     {
       "handle": "standardpuppies",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2022-10-31T02:41:12.896814"
+      "timestamp": "2022-10-31T20:14:17.013156"
     },
     {
       "handle": "PuppiesIover",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2022-10-31T02:41:14.934203"
+      "timestamp": "2022-10-31T20:14:19.044017"
     },
     {
       "handle": "DailyManedWolf",
       "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-10-31T02:41:38.663543"
+      "timestamp": "2022-10-31T20:14:42.326638"
     }
   ]
 }
