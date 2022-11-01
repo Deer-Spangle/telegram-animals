900bfdcefb2b9dbbb0530cfe656c36ce5cceb899
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2022-11-01 10:07:04 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@0410a9336bee3a2b3e3c1d1fa1714ade905456f2 🚀

--- errors.js
+++ errors.js
@@ -1,36 +1,30 @@
 const telegramChannels = {
-  "telegram_cache_errors": [
-    {
-      "handle": "bagofcheetos",
-      "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by GetChannelsRequest)",
-      "timestamp": "2022-11-01T08:27:53.736002"
-    }
-  ],
+  "telegram_cache_errors": [],
   "twitter_cache_errors": [
     {
       "handle": "batsdotexe",
       "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-11-01T08:28:01.942985"
+      "timestamp": "2022-11-01T10:02:45.562602"
     },
     {
       "handle": "CuteLilRedBois",
       "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-11-01T08:28:04.578752"
+      "timestamp": "2022-11-01T10:06:05.198913"
     },
     {
       "handle": "standardpuppies",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2022-11-01T08:28:06.717212"
+      "timestamp": "2022-11-01T10:06:14.575144"
     },
     {
       "handle": "PuppiesIover",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2022-11-01T08:28:08.819868"
+      "timestamp": "2022-11-01T10:06:16.607416"
     },
     {
       "handle": "DailyManedWolf",
       "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-11-01T08:28:10.917956"
+      "timestamp": "2022-11-01T10:06:36.562752"
     }
   ]
 }
