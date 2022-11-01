e0883c42af3e72773f8b338e4e457c81fdb33e1e
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2022-11-01 19:52:13 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@dce57896dcdd8a815c3bc2d01a8779f0a3959079 🚀

--- errors.js
+++ errors.js
@@ -1,30 +1,4 @@
 const telegramChannels = {
   "telegram_cache_errors": [],
-  "twitter_cache_errors": [
-    {
-      "handle": "batsdotexe",
-      "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-11-01T15:04:06.085044"
-    },
-    {
-      "handle": "CuteLilRedBois",
-      "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-11-01T15:04:08.625279"
-    },
-    {
-      "handle": "standardpuppies",
-      "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2022-11-01T15:04:10.680906"
-    },
-    {
-      "handle": "PuppiesIover",
-      "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2022-11-01T15:04:12.713668"
-    },
-    {
-      "handle": "DailyManedWolf",
-      "error": "Cache update failed: [{'code': 50, 'message': 'User not found.'}]",
-      "timestamp": "2022-11-01T15:04:14.800631"
-    }
-  ]
+  "twitter_cache_errors": []
 }
