a51ecf6a7624d8d9b008a8f9e9b6838ad4b5a447
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2022-11-20 02:16:08 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@3ed659c8605fa747c5b1b396d4c649c8f5d06706 🚀

--- errors.js
+++ errors.js
@@ -1,4 +1,10 @@
 const telegramChannels = {
-  "telegram_cache_errors": [],
+  "telegram_cache_errors": [
+    {
+      "handle": "puppygif",
+      "error": "Cache update failed: Channel handle should be puppygif but it is uwontbelieve",
+      "timestamp": "2022-11-20T02:07:12.527933"
+    }
+  ],
   "twitter_cache_errors": []
 }
