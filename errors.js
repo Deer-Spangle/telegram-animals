936ca5e1b8700fccbfebb1fceda578d42b0121c7
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2023-01-23 00:54:55 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@8aaf10b874c20e54929a636b782b74475fd11f4f 🚀

--- errors.js
+++ errors.js
@@ -1,16 +1,21 @@
 const telegramChannels = {
   "telegram_cache_errors": [
     {
+      "handle": "froggosfroggies",
+      "error": "Cache update failed: Channel handle should be froggosfroggies but it is vivalerane",
+      "timestamp": "2023-01-23T00:45:32.302515"
+    },
+    {
       "handle": "puppygif",
       "error": "Cache update failed: Channel handle should be puppygif but it is uwontbelieve",
-      "timestamp": "2023-01-22T01:48:10.371906"
+      "timestamp": "2023-01-23T00:46:26.352999"
     }
   ],
   "twitter_cache_errors": [
     {
       "handle": "platinumfoxes",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2023-01-22T01:49:11.871547"
+      "timestamp": "2023-01-23T00:47:32.140834"
     }
   ]
 }
