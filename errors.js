00a852513706c8d69570e1e2c5cc8e0029f99a3d
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2023-04-15 01:25:32 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@1aaf7bf8dc16c0c9b88ff2f582999707ecc6630b 🚀

--- errors.js
+++ errors.js
@@ -3,34 +3,29 @@ const telegramChannels = {
     {
       "handle": "froggosfroggies",
       "error": "Cache update failed: Channel handle should be froggosfroggies but it is vivalerane",
-      "timestamp": "2023-04-14T21:46:46.899986"
+      "timestamp": "2023-04-15T01:25:15.959027"
     },
     {
       "handle": "puppygif",
       "error": "Cache update failed: Channel handle should be puppygif but it is uwontbelieve",
-      "timestamp": "2023-04-14T21:47:39.644542"
+      "timestamp": "2023-04-15T01:25:16.078927"
     },
     {
       "handle": "reptilepets",
       "error": "Cache update failed: Channel handle should be reptilepets but it is joeperronsreptile",
-      "timestamp": "2023-04-14T21:47:42.472451"
+      "timestamp": "2023-04-15T01:25:16.200615"
     }
   ],
   "twitter_cache_errors": [
     {
       "handle": "platinumfoxes",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2023-04-14T21:48:40.342731"
+      "timestamp": "2023-04-15T01:25:19.145907"
     },
     {
       "handle": "cat_dot_exe",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2023-04-14T21:50:16.339865"
-    },
-    {
-      "handle": "CrowMonthly",
-      "error": "Cache update failed: [{'message': 'Internal error', 'code': 131}]",
-      "timestamp": "2023-04-14T21:54:39.669293"
+      "timestamp": "2023-04-15T01:25:21.399146"
     }
   ]
 }
