f3bb465956569c885693efa4f88ec05fb7f8b28b
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2023-06-10 01:52:07 +0000

    Deploying to gh-pages from @ Deer-Spangle/telegram-animals@ede5e9ea74c2cd7e3d76361341ea5e5d2bdde114 🚀

--- errors.js
+++ errors.js
@@ -3,34 +3,34 @@ const telegramChannels = {
     {
       "handle": "otterpics",
       "error": "Cache update failed: The channel specified is private and you lack permission to access it. Another reason may be that you were banned from it (caused by GetChannelsRequest)",
-      "timestamp": "2023-06-09T01:50:36.783219"
+      "timestamp": "2023-06-10T01:41:10.782997"
     },
     {
       "handle": "froggosfroggies",
       "error": "Cache update failed: Channel handle should be froggosfroggies but it is vivalerane",
-      "timestamp": "2023-06-09T01:52:51.026236"
+      "timestamp": "2023-06-10T01:43:23.921407"
     },
     {
       "handle": "puppygif",
       "error": "Cache update failed: Channel handle should be puppygif but it is uwontbelieve",
-      "timestamp": "2023-06-09T01:53:43.750217"
+      "timestamp": "2023-06-10T01:44:16.536807"
     },
     {
       "handle": "reptilepets",
       "error": "Cache update failed: Channel handle should be reptilepets but it is joeperronreptiles",
-      "timestamp": "2023-06-09T01:53:45.952902"
+      "timestamp": "2023-06-10T01:44:18.797734"
     }
   ],
   "twitter_cache_errors": [
     {
       "handle": "platinumfoxes",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2023-06-09T01:54:44.101430"
+      "timestamp": "2023-06-10T01:45:16.174474"
     },
     {
       "handle": "cat_dot_exe",
       "error": "Cache update failed: [{'code': 63, 'message': 'User has been suspended.'}]",
-      "timestamp": "2023-06-09T01:56:30.395627"
+      "timestamp": "2023-06-10T01:46:59.920917"
     }
   ]
 }
